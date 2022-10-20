c90321f70391f392c4b369e48eb726da1196ccf0
RomanCLV <roman.clavier.2001@gmail.com>
2022-10-20 21:48:31 +0200

    add base folders and class

--- BezierCurves/Views/MainWindow.xaml.cs
+++ BezierCurves/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BezierCurves
+namespace BezierCurves.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
